64c86672d5cdcbf19a01df7b593264fb1669774b
Mateusz Bronclik <mateuszbronclik@gmail.com>
2022-03-25 17:11:10 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jak Ci na imie?");
+            Console.Write("Napisz tutaj swoje imie: ");
+            string imie = Console.ReadLine();
+            if (imie.Length == 0)
+            {
+                Console.Error.WriteLine("\n\n\t*** Blad: nie podano imienia!\n\n");
+                return;
+            }
+            bool niewiasta = imie.ToLower()[imie.Length - 1] == 'a';
+            if (imie == "Kuba" || imie == "Barnaba" || imie == "kuba" || imie == "barnaba") niewiasta = false;
+            Console.WriteLine(
+                "Niech zgadne, jestes " + (niewiasta ? "dziewczyna " : "chlopakiem ") + "!");
+
+            Console.WriteLine("Nacisnij Enter...");
+            Console.Read();
+        }
+    }
+}
